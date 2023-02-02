b1cfbd7fa7725965ad4e099e6b9c53ba67d654a1
Skyler Austen <me@skylerausten.com>
2023-02-02 03:23:15 -0600

    Tinkering with git stuff

---
+++ math.py
@@ -0,0 +1,16 @@
+# 1 / 31 Class Code
+
+# Math Operators Review
+seconds_per_minute = 60
+minutes_per_hour = 60
+hours_per_day = 24
+
+# Formula for seconds per hour.
+# seconds_per_hour = 
+# seconds_per_day = 
+print("Seconds per day:", seconds_per_day)
+
+# Compare / and //.
+print("Seconds per hour: ", seconds_per_day / seconds_per_hour, "\n")
+print("Seconds per seconds_per_day // seconds_per_hour)
+
